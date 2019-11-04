0d8b59e14ca0c77c06f45b9745eafffbc96dcecf
Tj <tj@tj>
2019-11-04 22:50:04 +0800

    save the MAC and IPS

--- network/server.go
+++ network/server.go
@@ -2,12 +2,16 @@ package network
 
 import "net"
 
-func server()  {
+func boardCast()  {
+	//local
 	// 这里设置发送者的IP地址，自己查看一下自己的IP自行设定
 	laddr := net.UDPAddr{
-		IP:   net.IPv4(192, 168, 137, 224),
+		//IP:   net.IPv4(192, 168, 137, 224),
+		//IP:   net.IPv4(0, 0, 0, 0),
+		IP: net.IPv4zero,
 		Port: 3000,
 	}
+	//remote
 	// 这里设置接收者的IP地址为广播地址
 	raddr := net.UDPAddr{
 		IP:   net.IPv4(255, 255, 255, 255),
@@ -20,4 +24,8 @@ func server()  {
 	}
 	conn.Write([]byte(`hello peers`))
 	conn.Close()
+}
+
+func client(){
+
 }
